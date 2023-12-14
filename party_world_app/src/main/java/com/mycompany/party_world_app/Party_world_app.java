b58ded9a0c85428b949d94b0faac65ccd93b2837
ZEHRA BENGÜ EMÜL <bengu.emul@std.yildiz.edu.tr>
2023-12-14 16:55:05 +0300

    file_upload

---
+++ party_world_app/src/main/java/com/mycompany/party_world_app/Party_world_app.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.party_world_app;
+
+/**
+ *
+ * @author ZEHRABENGÜEMÜL
+ */
+public class Party_world_app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
